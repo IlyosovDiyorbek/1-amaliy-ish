63dac54825d64aa212bcbf64f4937cff96302fec
Ilyosov <119863947+IlyosovDiyorbek@users.noreply.github.com>
2022-12-20 11:09:28 +0500

    Add files via upload

---
+++ 2(2)-amaliy ishi.py
@@ -0,0 +1,9 @@
+a=int(input(' uchburchakni a tomonini kiriting a='))
+b=int(input(' uchburchakni b tomonini kiriting b='))
+c=int(input(' uchburchakni c tomonini kiriting c='))
+if a==b or a==c or b==c:
+    print('uchburchak teng yonli')
+else :
+    print(('teng yonli emas'))
+
+
